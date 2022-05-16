ab81be23ea343ae35831176e08df3d095fa5dca2
Enrico <enrico.bonato95@gmail.com>
2022-05-16 22:14:14 +0200

    helper

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
+import ProjectCard from "./ProjectCards";import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import { GetRepoData } from "./GetRepoData"
+
 
 function Projects() {
   return (
     <Container fluid className="project-section">
       <Particle />
+      <div>
+      <GetRepoData
+        showStars={true}
+        numOfrepos={6}
+        userName={"EnricoBonato-web"}
+        showLanguage={"en"}
+      />
+    </div>
       <Container>
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
