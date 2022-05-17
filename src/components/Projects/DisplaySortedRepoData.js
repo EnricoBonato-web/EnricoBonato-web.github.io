252615a9b1b688a9171d9dc880d3021db5fc61c3
Enrico <enrico.bonato95@gmail.com>
2022-05-17 16:41:27 +0200

    ordered

--- src/components/Projects/DisplaySortedRepoData.js
+++ src/components/Projects/DisplaySortedRepoData.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { sortByMostRecentDate } from '../helper/sortByMostRecentDate';
-import { arrayToLength } from '../helper/arrayToLength';
 import { Col } from 'react-bootstrap';
 import ProjectCards from './ProjectCards';
-import { Container, Row } from 'react-bootstrap';
+import {  Row } from 'react-bootstrap';
 
 function DisplaySortedRepoData({ repoData, numOfrepos, showStars, showLanguage }) {
-  console.log('sorted' + repoData);
   const text = (
     <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
-      {repoData.map(repo => {
+      {repoData.sort((a,b)=>a.id<b.id?1:-1).map(repo => {
         return (
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" key={repo.id}>
             <ProjectCards
               key={repo.id}
               title={repo.name}
               description={repo.description}
               link={repo.html_url}
+              imgPath={repo.language}
             ></ProjectCards>
           </Col>
         );
--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,21 +1,32 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { BiLinkExternal } from "react-icons/bi";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { BiLinkExternal } from 'react-icons/bi';
 
 function ProjectCards(props) {
+  let img = props.imgPath;
+  img = img.toLocaleLowerCase();
+  if (img === 'html') img = 'html5'.toLocaleUpperCase();
+  if (img === 'css') img = 'css3'.toLocaleUpperCase();
   return (
     <Card className="project-card-view">
-      {console.log("card:"+props.title)}
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img
+        variant="top"
+        src={(
+          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/' +
+          img +
+          '/' +
+          img +
+          '-original.svg'
+        ).toLocaleLowerCase()}
+        alt={img}
+      />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
+        <Card.Text style={{ textAlign: 'justify' }}>{props.description}</Card.Text>
         <Button variant="primary" href={props.link} target="_blank">
           <BiLinkExternal /> &nbsp;
-          {props.isBlog ? "View Blog" : "View Project"}
+          {props.isBlog ? 'View Blog' : 'View Project'}
         </Button>
       </Card.Body>
     </Card>
