cdef67243c928145447f57b85a4a71d9cdd387ba
Enrico <enrico.bonato95@gmail.com>
2022-05-18 12:44:44 +0200

    die

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/EnricoBonato-web"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/EnricoBonato-web/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
