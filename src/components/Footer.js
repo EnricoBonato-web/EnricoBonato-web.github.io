263ce1068de38500d3704e584967a4dc32e1f591
Enrico <enrico.bonato95@gmail.com>
2022-05-18 02:51:06 +0200

    build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Template by Soumyajit Behera<br/> implemented by Enrico Bonato</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} EB</h3>
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/EnricoBonato-web"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
+                href="https://www.instagram.com/booonnyy/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
