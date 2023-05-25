f6746a1d3b8a8d8de77a801b7c050df02153074d
EnricoBonato-web <enrico.bonato95@gmail.com>
2023-05-25 11:26:01 +0200

    cose

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,19 +1,19 @@
-import React, { FC } from "react";
-import style from "../styles/about.module.scss";
+import React, { FC } from 'react';
+import style from '../styles/about.module.scss';
+import Planet from './Planet';
 
-interface AboutProps {}
-
-const About: FC<AboutProps> = () => (
+const About: FC = () => (
   <div className={style.container}>
-    <h1> Something about me</h1>
-    <p>
-      I am a frontend developer with a passion for crafting intuitive user
-      interfaces. Throughout my career, I have honed my skills in HTML, CSS, and
-      JavaScript to create visually appealing and functional websites. My
-      experience includes working on various projects, from online stores to
-      enterprise resource planning systems. I am detail-oriented, driven, and
-      always eager to learn and improve my craft.
-    </p>
+    <Planet planet="venus" planetSize={100}>
+      <h1> Something about me</h1>
+      <p>
+        I am a frontend developer with a passion for crafting intuitive user interfaces. Throughout
+        my career, I have honed my skills in HTML, CSS, and JavaScript to create visually appealing
+        and functional websites. My experience includes working on various projects, from online
+        stores to enterprise resource planning systems. I am detail-oriented, driven, and always
+        eager to learn and improve my craft.
+      </p>
+    </Planet>
   </div>
 );
 
